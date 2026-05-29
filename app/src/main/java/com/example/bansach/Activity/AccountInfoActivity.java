50b326d6bb6ecfd1ae3371af65d98c3e8e50f376
Tran Cong Vinh <23130384@st.hcmuaf.edu.vn>
2026-05-29 10:02:59 +0700

    add account info

---
+++ app/src/main/java/com/example/bansach/Activity/AccountInfoActivity.java
@@ -0,0 +1,15 @@
+package com.example.bansach.Activity;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.bansach.R;
+
+public class AccountInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_info);
+    }
+}
